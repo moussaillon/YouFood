9ff388c696d9a46442eef88e50a1ee1af0d91cff
Jean Inderchit <jean.inderchit@gmail.com>
2012-02-17 12:22:35 +0100

    Changing index.php.

--- www/index.php
+++ www/index.php
@@ -1,8 +1,13 @@
+<!DOCTYPE HTML>
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+
+	<head>
+	    <link rel="icon" type="image/png" href="favicon.ico" />
+        <title>YouFood</title>
+    </head>
+    
+    <body>
+        <?php echo '<p>Bienvenue sur le site de YouFood</p>'; ?> 
+    </body>
+    
 </html>
